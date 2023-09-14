eb355873f1f16efab0b068c982e173840fdc9f1b
jonathanbtellez <jonathanbtellez@gmail.com>
2023-09-14 09:44:43 -0500

    CRUD category using composables and implement category modal

--- resources/js/components/category/CategoriesList.vue
+++ resources/js/components/category/CategoriesList.vue
@@ -1,44 +1,46 @@
-<template >
-	<div class="container">
-
-		<div class="card bg-light">
-			<div class="card-header d-flex justify-content-end">
-				<button class="btn btn-primary" @click="createCategory">Crear categoria</button>
-			</div>
-			<div class="card-body">
-				<div class="card-body table-responsive">
-					<table class="table table-bordered table-striped table-hover table-light " id="category_table">
-						<thead class="text-center">
-							<tr>
-								<th>Nombre</th>
-								<th>Acciones</th>
-							</tr>
-						</thead>
-						<tbody @click="handleAction">
-						</tbody>
-					</table>
-				</div>
-			</div>
+<template>
+	<div class="card  bg-light container">
+		<div class="card-header d-flex justify-content-end">
+			<button class="btn btn-primary" @click="createCategory">Crear categoria</button>
 		</div>
-		<div v-if="load_modal">
-			<h1>Hola Mundo</h1>
-			<!-- <category-modal :category_data="category" /> -->
+		<div class="card-body">
+			<div class="table-responsive my-4 mx-2">
+				<table class="table table-bordered table-light table-striped" id="category_table">
+					<thead>
+						<tr>
+							<th>Nombre</th>
+							<th>Acciones</th>
+						</tr>
+					</thead>
+					<tbody @click="handleAction">
+					</tbody>
+				</table>
+			</div>
 		</div>
 	</div>
+	<div v-if="load_modal">
+		<category-modal :category_data="category" />
+	</div>
 </template>
+
 <script>
-
-import { ref, onMounted } from 'vue';
-import { deleteMessage, handlerErrors , successMessage } from '@/helpers/Alerts';
-
+import { handlerErrors, successMessage, deleteMessage } from '@/helpers/Alerts.js'
+import HandlerModal from '@/helpers/HandlerModal.js'
+import CategoryModal from './CategoryModal.vue'
+import { ref, onMounted } from 'vue'
 export default {
-	// props:[],
-	setup(/* props, context*/) { /* Se monta en beforeCreated */
-
-		const load_modal = ref(false)
+	components: { CategoryModal },
+	/*
+	* Setup se lanza en beforeCreated
+	*/
+	setup(/* props, context*/) {
+		const categories = ref(null)
+		const category = ref(null)
 		const table = ref(null)
-		const category = ref({})
+		const { openModal, closeModal, load_modal } = HandlerModal()
 
+		onMounted(() => index());
+		const index = () => mountedTable()
 
 		const mountedTable = () => {
 			table.value = $('#category_table').DataTable({
@@ -49,42 +51,52 @@ export default {
 				order: [[0, 'asc']],
 				autoWidth: false,
 				dom: 'Bfrtip',
-				buttons: ['pageLength', 'excel', 'pdf', 'copy'],
-				// datatable backend
+				buttons: ['colvis', 'pageLength', 'excel', 'pdf', 'copy'],
 				ajax: `/categories/get-all-dt`,
 				columns: [
 					{ data: 'name', name: 'name', orderable: true, searchable: true },
-					{	name: 'action',
+					{
+						name: 'action',
 						orderable: false,
 						searchable: false,
 						render: (data, type, row, meta) => {
 							return `<div class="d-flex justify-content-center" data-role='actions'>
-		            					<button onclick='event.preventDefault();' data-id='${row.id}' role='edit' class="btn btn-warning btn-sm">
-		              					<i class='fas fa-pencil-alt' data-id='${row.id}' role='edit'></i>
-										</button>
-		            					<button onclick='event.preventDefault();' data-id='${row.id}' role='delete' class="btn btn-danger btn-sm ms-1">
-		            					<i class='fas fa-trash-alt' data-id='${row.id}' role='delete'></i>
-										</button>
-		          					</div>`	}
+		            <button onclick='event.preventDefault();' data-id='${row.id}' role='edit' class="btn btn-warning btn-sm">
+		              <i class='fas fa-pencil-alt' data-id='${row.id}' role='edit'></i>
+								</button>
+		            <button onclick='event.preventDefault();' data-id='${row.id}' role='delete' class="btn btn-danger btn-sm ms-1">
+		            	<i class='fas fa-trash-alt' data-id='${row.id}' role='delete'></i>
+								</button>
+		          </div>`
+						}
 					}
 				]
 			})
 		}
 
-		const createCategory = () => {
-			load_modal.value = !load_modal.value
+		const handleAction = (event) => {
+			const button = event.target
+			const category_id = button.getAttribute('data-id')
+			if (button.getAttribute('role') == 'edit') {
+				editCategory(category_id)
+			} else if (button.getAttribute('role') == 'delete') {
+				deleteCategory(category_id)
+			}
 		}
 
 		const editCategory = async (id) => {
 			try {
-				console.log(id);
 				const { data } = await axios.get(`/categories/${id}`)
 				category.value = data.category
-				console.log(category.value);
-				// await openModal('category_modal')
+				await openModal('category_modal')
 			} catch (error) {
 				await handlerErrors(error)
 			}
+		}
+
+		const createCategory = async () => {
+			category.value = null
+			await openModal('category_modal')
 		}
 
 		const deleteCategory = async (id) => {
@@ -92,32 +104,27 @@ export default {
 			try {
 				await axios.delete(`/categories/${id}`)
 				await successMessage({ is_delete: true })
-				// reloadState()
+				reloadState()
 			} catch (error) {
 				await handlerErrors(error)
 			}
 		}
 
+		const reloadState = () => {
+			table.value.destroy()
+			index()
+		}
 
-
-		const handleAction = (event) => {
-			const button = event.target
-			const category_id = button.getAttribute('data-id')
-
-			if (button.getAttribute('role') == 'edit') {
-				editCategory(category_id)
-			} else if (button.getAttribute('role') == 'delete') {
-				deleteCategory(category_id)
-			}
-		 }
-
-		onMounted(() => {
-			mountedTable()
-		})
-
+		// index()
 		return {
+			categories,
+			category,
 			load_modal,
+			editCategory,
+			deleteCategory,
 			createCategory,
+			closeModal,
+			reloadState,
 			handleAction
 		}
 	}
